3f14aaf4e9017948d0277adce6b03ec78f8af9e9
johan ospina <apoyodesarrolloingenieria3@udea.edu.co>
2019-01-31 14:45:28 -0500

    Algoritmos curso de javascript

---
+++ arreglosObjeto.js
@@ -0,0 +1,7 @@
+let arra = new Array(2,3,51,4,355,23,12);
+
+console.log(arra[2]);
+
+let aux = arra[4];
+
+console.log(aux);
---
+++ operaciones.js
@@ -0,0 +1,9 @@
+function suma(a,b){
+    let c = a+b;
+    return c;
+}
+
+suma1=()=> {return a+b;}
+
+console.log(suma(10,5));
+
