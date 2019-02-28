bc82239db3e6a19244ef8ee42b03c1a84f593468
jeroquendo <jeroquendo@gmail.com>
2019-02-28 17:25:14 -0500

    c
    C

---
+++ prototype.js
@@ -0,0 +1,20 @@
+"use strict"
+    
+function animal(patas, nombre,peso, raza) { 
+    this.patas = patas
+    this.nombre = nombre
+    this.peso = peso
+    this.raza = raza
+    let getRaza = (raza1, raza2) => {
+        if (this.raza == raza1) return raza1;
+        else   return raza2
+   }
+}
+let perros = () => {
+    var perro = new animal(4, Orus, 12, chanda) 
+    var perro1 = new animal(4, Dante2, 14, buenaRaza)
+    var perro2 = new animal(4, Dante, 5, buenaRaza)
+
+    
+    }
+
